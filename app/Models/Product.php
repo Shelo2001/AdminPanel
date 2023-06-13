7532516cd4899f04fa2f4e4dad8458f8dfb841ac
Shelo2001 <sheliaakaki47@gmail.com>
2023-06-13 16:47:18 +0400

    added migrations, controller and seeder

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\ProductImage;
+use App\Models\ProductCategories;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+    public function Images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function Categories()
+    {
+        return $this->hasMany(ProductCategories::class);
+    }
+}
--- database/migrations/2023_06_13_123617_create_product_categories_table.php
+++ database/migrations/2023_06_13_123617_create_product_categories_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('category');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_categories');
     }
 };
