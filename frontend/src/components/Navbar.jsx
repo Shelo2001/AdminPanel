7bf159213db936cdb7733516a607de761686f82e
Shelo2001 <sheliaakaki47@gmail.com>
2023-06-13 12:23:49 +0400

    added layout and pages in frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import { DesktopOutlined, PieChartOutlined } from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
+import { useState } from "react";
+import { Outlet } from "react-router";
+import { Link, useLocation } from "react-router-dom";
+const { Header, Content, Sider } = Layout;
+function getItem(label, key, icon, to) {
+    return {
+        label,
+        key,
+        icon,
+        to,
+    };
+}
+const items = [
+    getItem("Home", "1", <PieChartOutlined />, "/products"),
+    getItem("Admin Panel", "2", <DesktopOutlined />, "/adminpanel"),
+];
+
+const Navbar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+    return (
+        <Layout
+            style={{
+                minHeight: "100vh",
+            }}
+        >
+            <Sider
+                collapsible
+                collapsed={collapsed}
+                onCollapse={(value) => setCollapsed(value)}
+            >
+                <div className="demo-logo-vertical" />
+                <Menu
+                    theme="dark"
+                    defaultSelectedKeys={
+                        window.location.pathname === "/adminpanel"
+                            ? ["2"]
+                            : ["1"]
+                    }
+                    mode="inline"
+                >
+                    {items.map((item) => (
+                        <Menu.Item key={item.key} icon={item.icon}>
+                            <Link to={item.to}>{item.label}</Link>
+                        </Menu.Item>
+                    ))}
+                </Menu>
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: "0 16px",
+                    }}
+                >
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                            background: colorBgContainer,
+                        }}
+                    >
+                        <Outlet />
+                    </div>
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+export default Navbar;
